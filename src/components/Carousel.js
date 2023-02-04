4a1a66e912aff336a948dfb1726ea4affc80a872
Jia-Yi Jian <jyjian@illinois.edu>
2023-02-03 16:15:23 -0800

    make slider image responsive

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -8,6 +8,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
 import 'swiper/css/scrollbar';
+import './Carousel.css';
 
 
 class Carousel extends Component {
