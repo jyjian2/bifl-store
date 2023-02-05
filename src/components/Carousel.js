c5818ad3a929b7d476515f73802141b3600c6f89
Jia-Yi Jian <jyjian@illinois.edu>
2023-02-04 21:01:05 -0800

    make carousel image responsive

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,10 +1,7 @@
-import React, {Component} from "react";
-import { Navigation, Pagination, Autoplay, Scrollbar} from 'swiper';
+import React, { Component } from "react";
+import { Navigation, Pagination, Autoplay, Scrollbar } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import slideImg1 from '../images/slideImg1.jpg';
-import slideImg2 from '../images/slideImg2.jpg';
-import slideImg3 from '../images/slideImg3.jpg';
-import slideImg4 from '../images/slideImg4.jpg';
+
 
 
 
@@ -24,30 +21,32 @@ class Carousel extends Component {
         const slideImagesNames = ["slideImg1", "slideImg2", "slideImg3", "slideImg4"]
 
         return (
-            
-            <Swiper 
-            modules={[Navigation, Pagination, Autoplay, Scrollbar]}
-            spaceBetween={0}
-            slidesperview={3}
-            navigation={true}
-            autoplay={{
-                delay: 4000,
-                disableOnInteraction: false,
-            }}
-            pagination={{ clickable: true }}
-            scrollbar={{ draggable: true }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() =>  console.log('slide change')}
-            >
-            {slideImagesNames.map(image=> 
-            <SwiperSlide>
-            <img class="slide_image" src={require(`../images/${image}.jpg`)}></img>
-            </SwiperSlide>)}
-            
-    
-            </Swiper>
-            
-            
+            <div className="section-banner">
+                <Swiper
+                    modules={[Navigation, Pagination, Autoplay, Scrollbar]}
+                    spaceBetween={0}
+                    slidesperview={3}
+                    navigation={true}
+                    autoplay={{
+                        delay: 4000,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{ clickable: true }}
+                    scrollbar={{ draggable: true }}
+                    onSwiper={(swiper) => console.log(swiper)}
+                    onSlideChange={() => console.log('slide change')}
+                >
+                    {slideImagesNames.map(image =>
+                        <SwiperSlide>
+                            <img className="slide_image" src={require(`../images/${image}.jpg`)}></img>
+                        </SwiperSlide>)}
+
+
+                </Swiper>
+            </div>
+
+
+
         );
     }
 }
