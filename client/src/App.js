03f36e13c7c0ccf8f02279b6043473aefc24d8c5
Jia-Yi Jian <jyjian@illinois.edu>
2023-02-14 13:46:43 -0800

    rearrage files

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Carousel from './components/Carousel';
+import BrandCard from './components/BrandCard';
+import React, {Component} from 'react';
+
+class App extends Component {
+    
+    render() {
+
+        return (
+
+                <React.Fragment>
+                <Carousel />
+                <BrandCard />
+                </React.Fragment>
+            );
+    }
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const app = document.getElementById('app');
+
+// create a root
+const root = createRoot(app);
+
+//render app to root
+root.render(<App />);
