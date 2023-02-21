25223bff6105af07be5f350632897ca25c93fb89
Jia-Yi Jian <jyjian@illinois.edu>
2023-02-21 14:53:33 -0800

    npm install axios

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom';
 
 
 const app = document.getElementById('app');
