dc1997e164f56802b97d7818499e3f7d6bab59ad
Jia-Yi Jian <jyjian@illinois.edu>
2023-02-22 22:12:51 -0800

    refactor brand card

--- src/components/BrandCard.js
+++ src/components/BrandCard.js
@@ -1,47 +1,19 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 import faribaultImg from '../images/faribault.jpeg';
 import './BrandCard.css';
 
 export default function BrandCard(props) {
+    const brand = props.brand
+    return (
 
-    const displayBrands = (props) => {
-        const {brands} = props;
-
-        if(brands.length > 0) {
-            return(
-                brands.map((brand, index) => {
-                    console.log(brand);
-                    return(
-                        <div class="featured-brand-cards" >
-                            <Row xs={1} md={2} lg={4}>
-                            <Col>
-                                    <Card style={{width: '18rem'}} className="my-3 mx-auto" key={brand._id}>
-                                        <Card.Img variant="top" src={faribaultImg} />
-                                        <Card.Body>
-                                            <Card.Title>{brand.name}</Card.Title>
-                                            <Card.Text>{brand.description}</Card.Text>
-                                            <Button variant="primary">See porducts from this brand</Button>            
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                            </Row>
-                        </div>
-                    )
-                })
-            )
-        } else {
-            return (<h3>No brands yet</h3>)
-        }
-
-
-    }
-
-    return(
-        <>
-            {displayBrands(props)}
-        </>
+        <Card style={{ width: '18rem' }} className="my-3 mx-auto" key={brand._id}>
+            <Card.Img variant="top" src={faribaultImg} />
+            <Card.Body>
+                <Card.Title>{brand.name}</Card.Title>
+                <Card.Text>{brand.description}</Card.Text>
+                <Button variant="primary">See porducts from this brand</Button>
+            </Card.Body>
+        </Card>
     )
 }
--- src/components/ParentBrand.js
+++ src/components/ParentBrand.js
@@ -1,11 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import BrandCard from "./BrandCard";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 
 export default function ParentBrand() {
 
-    const [brands, setBrands] = useState('');
+    const [brands, setBrands] = useState([]);
 
     const url = 'http://localhost:3001/api/brands';
 
@@ -13,30 +15,41 @@ export default function ParentBrand() {
         getAllBrands();
     }, []);
 
+    console.log(brands);
     const getAllBrands = () => {
         axios.get(url)
-        .then((response) => {
-            if (response.data.success) {
-                const allBrands = response.data.data;
-                setBrands(allBrands);
-            } else {
-                console.error("response failed");
-            }
-        })
-        .catch(error => console.error(`Error: $(error)`));
+            .then((response) => {
+                if (response.data.success) {
+                    const allBrands = response.data.data;
+                    setBrands(allBrands);
+                } else {
+                    console.error("response failed");
+                }
+            })
+            .catch(error => console.error(`Error: $(error)`));
     }
 
-        return (
-            <section class="container-fluid brands-display">
-                <div class="featured-brands-header">
-                    <h2 class="featured-brands-text">
-                        Featured Brands
-                    </h2>
-                </div>
-                <div class="featured-brand-cards" >
-                    <BrandCard brands={brands}/>
-                </div>
-            </section>
-        )
+
+
+    return (
+        <section class="container-fluid brands-display">
+            <div class="featured-brands-header">
+                <h2 class="featured-brands-text">
+                    Featured Brands
+                </h2>
+            </div>
+            <div class="featured-brand-cards" >
+                <Row xs={1} md={2} lg={4}>
+                    {
+                        brands.map((brand, index) => {
+
+                            return <BrandCard brand={brand} />
+                        })
+                    }
+                </Row>
+
+            </div>
+        </section>
+    )
 }
 
