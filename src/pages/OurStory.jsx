e9dcb454f1f6c755f5c79b1a68b64c8d3dc7fcae
Jia-Yi Jian <jyjian@illinois.edu>
2023-03-02 17:18:55 -0800

    create brands component

--- src/pages/OurStory.jsx
+++ src/pages/OurStory.jsx
@@ -2,6 +2,6 @@ import React, {compoenent} from "react";
 
 export default function OurStory() {
     return (
-        <div>This page is about our story. FUCK</div>
+        <div>This page is about our story.</div>
     )
 }
