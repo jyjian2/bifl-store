d50acf0e98791a8570da083f4ae8f0567488e7f4
Jia-Yi Jian <jyjian@illinois.edu>
2023-03-04 13:35:11 -0800

    create add to cart link

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,3 +1,4 @@
+import { ButtonGroup, ButtonToolbar } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './ProductCard.css';
@@ -7,11 +8,12 @@ export default function ProductCard(props) {
     return (
 
         <Card style={{ width: '18rem' }} className="my-3 mx-auto" key={product._id}>
-            <Card.Img variant="top"/>
+            <Card.Img variant="top" />
             <Card.Body>
                 <Card.Title>{product.name}</Card.Title>
                 <Card.Text>{product.description} {product.price}</Card.Text>
-                <Button variant="primary">Porduct Detail</Button>
+                <Card.Link href='#'>Product Deatil</Card.Link>
+                <Card.Link href='#'>Add to Cart</Card.Link>
                 <Card.Footer className="text-muted">${product.price}</Card.Footer>
             </Card.Body>
         </Card>
